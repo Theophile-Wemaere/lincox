c101670f9b121d1c26ceaca763bf8456cd7d6e26
Theophile-Wemaere <theophile.wemaere@gmail.com>
2024-11-30 16:42:12 +0100

    added scope, ports and modes to cli args

--- src/toolbox.py
+++ src/toolbox.py
@@ -1,3 +1,6 @@
+from termcolor import colored
+from datetime import datetime
+
 import lincox as main
 
 DEBUG = False
@@ -22,8 +25,7 @@ def print_banner():
         By Theophile.W, Quentin.L, Maximilien.L and Maxime.V
         
 """
-    if not DEBUG:
-        print(banner)
+    print(banner)
 
 def exit_error(msg,code):
     """
@@ -33,10 +35,40 @@ def exit_error(msg,code):
     print(msg)
     exit(code)
 
+def get_header(type:str)->str:
+    """
+    get timestamp and type header
+    """
+
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    timestamp_header = '[' + colored(timestamp,"blue") + ']'
+    if type == "DEBUG":
+        return timestamp_header + ' ' + '['+colored("DEBUG",attrs=["bold"])+'] '
+
+    if type == "INFO":
+        return timestamp_header + ' ' + '['+colored("INFO","green")+'] '
+
+
 def debug(msg):
     """
     print debug message
     """
 
-    if DEBUG:
-        print("[DEBUG]",msg)
+    if not DEBUG:
+        return
+
+    print(get_header("DEBUG")+msg)
+
+def tprint(*args,end='\n'):
+    """
+    toolbox print with timestamp and colors
+    """
+    print(get_header("INFO")+" ".join(map(str, args)),end='')
+    print(end,end='')
+
+def isint(v):
+    try:     
+        i = int(v)
+    except:  
+        return False
+    return True
